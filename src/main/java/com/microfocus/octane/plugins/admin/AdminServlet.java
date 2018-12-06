4219a74e6a568aa61619e36ae6bb4d1c19ed8882
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-12-07 00:32:40 +0200

    add admin servlet

---
+++ src/main/java/com/microfocus/octane/plugins/admin/AdminServlet.java
@@ -0,0 +1,71 @@
+package com.microfocus.octane.plugins.admin;
+
+
+
+
+
+
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.templaterenderer.TemplateRenderer;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+
+@Scanned
+public class AdminServlet extends HttpServlet {
+
+	@ComponentImport
+	private final UserManager userManager;
+
+	@ComponentImport
+	private final LoginUriProvider loginUriProvider;
+
+	@ComponentImport
+	private final TemplateRenderer renderer;
+
+	@Inject
+	public AdminServlet(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer renderer)
+	{
+		this.userManager = userManager;
+		this.loginUriProvider = loginUriProvider;
+		this.renderer = renderer;
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+	{
+		String username = userManager.getRemoteUsername(request);
+		if (username == null || !userManager.isSystemAdmin(username))
+		{
+			redirectToLogin(request, response);
+			return;
+		}
+
+		response.setContentType("text/html;charset=utf-8");
+		renderer.render("templates/admin.vm", response.getWriter());
+	}
+
+	private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
+	}
+
+	private URI getUri(HttpServletRequest request)
+	{
+		StringBuffer builder = request.getRequestURL();
+		if (request.getQueryString() != null)
+		{
+			builder.append("?");
+			builder.append(request.getQueryString());
+		}
+		return URI.create(builder.toString());
+	}
+}
