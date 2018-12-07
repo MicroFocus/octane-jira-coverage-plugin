129e8a393f9cdd9c86d4d0fd721e5d92c93ef971
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-12-07 17:34:04 +0200

    adding rest

---
+++ src/main/java/com/microfocus/octane/plugins/admin/ConfigResource.java
@@ -0,0 +1,130 @@
+package com.microfocus.octane.plugins.admin;
+
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import com.atlassian.sal.api.transaction.TransactionCallback;
+import com.atlassian.sal.api.transaction.TransactionTemplate;
+import com.atlassian.sal.api.user.UserManager;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Path("/")
+@Scanned
+public class ConfigResource
+{
+    String PLUGIN_PREFIX = "com.microfocus.octane.plugins.";
+    String OCTANE_LOCATION_KEY = PLUGIN_PREFIX + "octaneUrl";
+    String CLIENT_ID_KEY = PLUGIN_PREFIX + "clientId";
+    String CLIENT_SECRET_KEY = PLUGIN_PREFIX + "clientSecret";
+
+    @ComponentImport
+    private final UserManager userManager;
+    @ComponentImport
+    private final PluginSettingsFactory pluginSettingsFactory;
+    @ComponentImport
+    private final TransactionTemplate transactionTemplate;
+
+    @Inject
+    public ConfigResource(UserManager userManager, PluginSettingsFactory pluginSettingsFactory,
+						  TransactionTemplate transactionTemplate)
+    {
+        this.userManager = userManager;
+        this.pluginSettingsFactory = pluginSettingsFactory;
+        this.transactionTemplate = transactionTemplate;
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@Context HttpServletRequest request)
+    {
+        String username = userManager.getRemoteUsername(request);
+        if (username == null || !userManager.isSystemAdmin(username))
+        {
+            return Response.status(Status.UNAUTHORIZED).build();
+        }
+
+        return Response.ok(transactionTemplate.execute(new TransactionCallback()
+        {
+            public Object doInTransaction()
+            {
+                PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
+                Config config = new Config();
+                config.setLocation((String) settings.get(OCTANE_LOCATION_KEY));
+                config.setClientId((String) settings.get(CLIENT_ID_KEY));
+                config.setClientSecret((String) settings.get(CLIENT_SECRET_KEY));
+
+                return config;
+            }
+        })).build();
+    }
+
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response put(final Config config, @Context HttpServletRequest request)
+    {
+        String username = userManager.getRemoteUsername(request);
+        if (username == null || !userManager.isSystemAdmin(username))
+        {
+            return Response.status(Status.UNAUTHORIZED).build();
+        }
+
+        transactionTemplate.execute(new TransactionCallback()
+        {
+            public Object doInTransaction()
+            {
+                PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
+                pluginSettings.put(OCTANE_LOCATION_KEY, config.getLocation());
+                pluginSettings.put(CLIENT_ID_KEY, config.getClientId());
+                pluginSettings.put(CLIENT_SECRET_KEY, config.getClientSecret());
+                return null;
+            }
+        });
+        return Response.noContent().build();
+    }
+
+    @XmlRootElement
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static final class Config
+    {
+        @XmlElement private String location;
+        @XmlElement private String clientId;
+        @XmlElement private String clientSecret;
+
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public String getClientId() {
+            return clientId;
+        }
+
+        public void setClientId(String clientId) {
+            this.clientId = clientId;
+        }
+
+        public String getClientSecret() {
+            return clientSecret;
+        }
+
+        public void setClientSecret(String clientSecret) {
+            this.clientSecret = clientSecret;
+        }
+    }
+}
