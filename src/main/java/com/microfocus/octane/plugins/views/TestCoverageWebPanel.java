78dd3b3b293f9c8d5f0785582304965c089c6629
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-02-06 22:38:24 +0200

    allow delete configuration

--- src/main/java/com/microfocus/octane/plugins/views/TestCoverageWebPanel.java
+++ src/main/java/com/microfocus/octane/plugins/views/TestCoverageWebPanel.java
@@ -71,7 +71,7 @@ public class TestCoverageWebPanel extends AbstractJiraContextProvider {
         //TEST TYPES
         TestStatusDescriptor passedStatus = new TestStatusDescriptor("passed", "list_node.run_status.passed", "Passed", "rgb(26, 172, 96)", 1);
         TestStatusDescriptor failedStatus = new TestStatusDescriptor("failed", "list_node.run_status.failed", "Failed", "rgb(229, 0, 76)", 2);
-        TestStatusDescriptor plannedStatus = new TestStatusDescriptor("planned", "list_node.run_status.planned", "Planned", "rgb(47, 214, 195)", 3);
+        TestStatusDescriptor plannedStatus = new TestStatusDescriptor("planned", "list_node.run_status.planned", "Planned", "#dddddd" /*"rgb(47, 214, 195)"*/, 3);
         TestStatusDescriptor skippedStatus = new TestStatusDescriptor("skipped", "list_node.run_status.skipped", "Skipped", "rgb(82, 22, 172)", 4);
         TestStatusDescriptor needAttentionStatus = new TestStatusDescriptor("needsAttention", "list_node.run_status.requires_attention", "Requires Attention", "rgb(252, 219, 31)", 5);
         TestStatusDescriptor testNoRunStatus = new TestStatusDescriptor("testNoRun", null, "Tests did not run", "#cccccc", 6);
