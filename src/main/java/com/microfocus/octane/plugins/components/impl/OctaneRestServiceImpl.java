d1ff3dae8814f3e97dde541c8fa61d2d78f3e17f
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-04-17 10:03:41 +0300

    defect #829259 : Jira plugin - Test coverage doesn't retrieve from octane

--- src/main/java/com/microfocus/octane/plugins/components/impl/OctaneRestServiceImpl.java
+++ src/main/java/com/microfocus/octane/plugins/components/impl/OctaneRestServiceImpl.java
@@ -87,7 +87,7 @@ public class OctaneRestServiceImpl implements OctaneRestService, OctaneConfigura
         //http://localhost:8080/api/shared_spaces/1001/workspaces/1002/runs/groups?query="test_of_last_run={product_areas={(id IN '2001')}}"&group_by=status
 
         String url = String.format(Constants.PUBLIC_API_WORKSPACE_LEVEL_ENTITIES, octaneConfiguration.getLocationParts().getSpaceId(), workspaceId, "runs/groups");
-        Map<String, String> headers = new HashMap<>();
+        Map<String, String> headers = createHeaderMapWithOctaneClientType();
         headers.put(RestConnector.HEADER_ACCEPT, RestConnector.HEADER_APPLICATION_JSON);
 
         OctaneQueryBuilder queryBuilder = OctaneQueryBuilder.create()
@@ -107,7 +107,7 @@ public class OctaneRestServiceImpl implements OctaneRestService, OctaneConfigura
         //https://localhost:8080/api/shared_spaces/1001/workspaces/1002/runs/groups?&query=%22test_of_last_run={covered_requirement={path=%270000000001OT0002K9*%27}};!test_of_last_run={null};status={null}%22&group_by=native_status
 
         String url = String.format(Constants.PUBLIC_API_WORKSPACE_LEVEL_ENTITIES, octaneConfiguration.getLocationParts().getSpaceId(), workspaceId, "runs/groups");
-        Map<String, String> headers = new HashMap<>();
+        Map<String, String> headers = createHeaderMapWithOctaneClientType();
         headers.put(RestConnector.HEADER_ACCEPT, RestConnector.HEADER_APPLICATION_JSON);
 
         OctaneQueryBuilder queryBuilder = OctaneQueryBuilder.create()
@@ -127,7 +127,7 @@ public class OctaneRestServiceImpl implements OctaneRestService, OctaneConfigura
         //http://localhost:8080/api/shared_spaces/1001/workspaces/1002/tests?fields=id&limit=1&query="((covered_content={(path='0000000000XC*')});((!(subtype='test_suite'))))"
 
         String url = String.format(Constants.PUBLIC_API_WORKSPACE_LEVEL_ENTITIES, octaneConfiguration.getLocationParts().getSpaceId(), workspaceId, "tests");
-        Map<String, String> headers = new HashMap<>();
+        Map<String, String> headers = createHeaderMapWithOctaneClientType();
         headers.put(RestConnector.HEADER_ACCEPT, RestConnector.HEADER_APPLICATION_JSON);
 
         String queryParam = OctaneQueryBuilder.create()
@@ -160,8 +160,7 @@ public class OctaneRestServiceImpl implements OctaneRestService, OctaneConfigura
                     octaneConfiguration.getLocationParts().getSpaceId(), workspaceId, collectionName);
         }
 
-        Map<String, String> headers = new HashMap<>();
-        addOctaneClientType(headers);
+        Map<String, String> headers = createHeaderMapWithOctaneClientType();
         headers.put(RestConnector.HEADER_ACCEPT, RestConnector.HEADER_APPLICATION_JSON);
 
         String responseStr = restConnector.httpGet(url, Arrays.asList(queryCondition), headers).getResponseData();
@@ -173,8 +172,7 @@ public class OctaneRestServiceImpl implements OctaneRestService, OctaneConfigura
     public List<String> getSupportedOctaneTypes(long workspaceId, String udfName) {
         long spaceId = octaneConfiguration.getLocationParts().getSpaceId();
         String entityCollectionUrl = String.format(Constants.PUBLIC_API_WORKSPACE_LEVEL_ENTITIES, spaceId, workspaceId, "metadata/fields");
-        Map<String, String> headers = new HashMap<>();
-        addOctaneClientType(headers);
+        Map<String, String> headers = createHeaderMapWithOctaneClientType();
         headers.put(RestConnector.HEADER_ACCEPT, RestConnector.HEADER_APPLICATION_JSON);
 
         QueryPhrase fieldNameCondition = new LogicalQueryPhrase("name", udfName);
@@ -188,8 +186,10 @@ public class OctaneRestServiceImpl implements OctaneRestService, OctaneConfigura
         return foundTypes;
     }
 
-    private void addOctaneClientType(Map<String, String> headers) {
+    private Map<String, String> createHeaderMapWithOctaneClientType() {
+        Map<String, String> headers = new HashMap<>();
         headers.put("HPECLIENTTYPE", "HPE_CI_CLIENT");
+        return headers;
     }
 
     @Override
@@ -197,8 +197,7 @@ public class OctaneRestServiceImpl implements OctaneRestService, OctaneConfigura
         //https://mqalb011sngx.saas.hpe.com/api/shared_spaces/3004/workspaces/2002/metadata/fields?&query=%22field_type=%27string%27;is_user_defined=true;(entity_name+IN+%27feature%27,%27application_module%27,%27requirement_document%27,%27story%27)%22
         long spaceId = octaneConfiguration.getLocationParts().getSpaceId();
         String entityCollectionUrl = String.format(Constants.PUBLIC_API_WORKSPACE_LEVEL_ENTITIES, spaceId, workspaceId, "metadata/fields");
-        Map<String, String> headers = new HashMap<>();
-        addOctaneClientType(headers);
+        Map<String, String> headers = createHeaderMapWithOctaneClientType();
         headers.put(RestConnector.HEADER_ACCEPT, RestConnector.HEADER_APPLICATION_JSON);
 
         String queryCondition = OctaneQueryBuilder.create()
