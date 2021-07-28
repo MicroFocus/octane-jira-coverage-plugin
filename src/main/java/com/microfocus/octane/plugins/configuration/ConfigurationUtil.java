6cc4ec679125c356d0184e6e7a60f71f19133bc5
Dan Moldoveanu <dan.moldoveanu@microfocus.com>
2021-07-28 14:23:12 +0300

    defect #1209123: catch IllegalArgument before Runtime exception

--- src/main/java/com/microfocus/octane/plugins/configuration/ConfigurationUtil.java
+++ src/main/java/com/microfocus/octane/plugins/configuration/ConfigurationUtil.java
@@ -193,6 +193,8 @@ public class ConfigurationUtil {
         } catch (RestStatusException e) {
             String msg = parseExceptionMessage(e,spaceConfig);
             throw new IllegalArgumentException(msg);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Test connection failed: " + e.getMessage()); //rethrow IllegalArgumentExceptions, so it can catch the Runtime ones
         } catch (RuntimeException e){
             throw new IllegalArgumentException("Test connection failed: Error occurred while trying to test the connection. Please check the host.");
         } catch (Exception e) {
