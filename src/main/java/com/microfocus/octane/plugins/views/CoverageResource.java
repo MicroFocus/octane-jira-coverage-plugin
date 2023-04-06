e807a316a111099c50843b179e88cdcb2dd28fbf
Dan Moldoveanu <dan.moldoveanu@microfocus.com>
2023-04-06 12:21:11 +0300

    story #2109026: add new dropdown on coverage for multiple octane workspace configurations

--- src/main/java/com/microfocus/octane/plugins/views/CoverageResource.java
+++ src/main/java/com/microfocus/octane/plugins/views/CoverageResource.java
@@ -19,6 +19,8 @@ import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.sal.api.user.UserProfile;
+import com.microfocus.octane.plugins.configuration.ConfigurationManager;
+import com.microfocus.octane.plugins.configuration.WorkspaceConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +30,10 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Consumes({MediaType.APPLICATION_JSON})
 @Produces({MediaType.APPLICATION_JSON})
@@ -47,12 +52,49 @@ public class CoverageResource {
     }
 
     @GET
-    public Response getCoverage(@Context HttpServletRequest request, @QueryParam("project-key") String projectKey, @QueryParam("issue-key") String issueKey, @QueryParam("issue-id") String issueId) {
+    public Response getCoverage(@Context HttpServletRequest request, @QueryParam("project-key") String projectKey, @QueryParam("issue-key") String issueKey, @QueryParam("issue-id") String issueId,
+                                @QueryParam("workspace-config-id") String workspaceConfigId, @QueryParam("workspace-id") String workspaceId) {
         UserProfile userProfile = userManager.getRemoteUser(request);
         if (userProfile == null) {
             return Response.status(Response.Status.UNAUTHORIZED).build();
         }
-        Map<String, Object> contextMap = CoverageUiHelper.buildCoverageContextMap(projectKey, issueKey, issueId);
+        Map<String, Object> contextMap = CoverageUiHelper.buildCoverageContextMap(projectKey, issueKey, issueId, workspaceConfigId, workspaceId);
         return Response.ok(contextMap).build();
+    }
+
+    @GET
+    @Path("/octane-workspaces")
+    public Response getOctaneWorkspaces(@Context HttpServletRequest request, @QueryParam("project-key") String projectKey, @QueryParam("issue-type") String issueType) {
+        UserProfile userProfile = userManager.getRemoteUser(request);
+        if (userProfile == null) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        List<WorkspaceConfiguration> workspaceConfigsForProjectKey = ConfigurationManager.getInstance().getWorkspaceConfigurations().stream()
+                .filter(wsc -> workspaceConfigContainsProjectAndIssueType(projectKey, issueType, wsc))
+                .collect(Collectors.toList());
+
+        List<Map<String, String>> wsConfigsMap = workspaceConfigsForProjectKey.stream()
+                .map(this::toMap)
+                .collect(Collectors.toList());
+
+        return Response.ok(wsConfigsMap).build();
+    }
+
+    private static boolean workspaceConfigContainsProjectAndIssueType(String projectKey, String issueType, WorkspaceConfiguration workspaceConfiguration) {
+        return workspaceConfiguration.getJiraProjects().stream().anyMatch(jiraProject -> jiraProject.equals(projectKey))
+                && workspaceConfiguration.getJiraIssueTypes().stream().anyMatch(jiraIssueType -> jiraIssueType.equals(issueType));
+    }
+
+    private Map<String, String> toMap(WorkspaceConfiguration wsc) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("id", wsc.getId());
+        map.put("spaceConfigId", wsc.getSpaceConfigurationId());
+        map.put("spaceConfigName", ConfigurationManager.getInstance().getSpaceConfigurationById(wsc.getSpaceConfigurationId(), false).get().getName());
+        map.put("workspaceId", String.valueOf(wsc.getWorkspaceId()));
+        map.put("workspaceName", wsc.getWorkspaceName());
+
+        return map;
     }
 }
